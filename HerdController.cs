f58497c547d9470f0fa14fe09d6047bd49592565
Elicarter20 <elicarter20@gmail.com>
2019-03-16 17:12:26 -0400

    Herd AI improved

--- HerdController.cs
+++ HerdController.cs
@@ -13,19 +13,23 @@ using System.Diagnostics;
  *      Seek out any grass TAG when hunger is empty              - works
  *          Replete hunger
  *          
- *      When lion in vision cone, run away                        - instead of vision cone, is smell distance...
+ *      When lion in SMELL DISTANCE, run away                        - 
  *          When lion gone a certain distance, stop running     - works, but with rigid body need to fix
  * 
- *      Random movement system                          - have YouTube video
+ *      Random movement system                          - have basic thing
  *     
  * 
  *      Herd following movement system              - implement LEADER state into switch statement that others will follow their WANDEr function
- *      Leader randomly assigned and fluctuates    - easy randomizer
+ *      Leader randomly assigned and fluctuates    - randomizer
  * 
  */
 
-    //current issues: stacking RIGID BODY BAD, instead need to trigger an actual 3rd person navmesh movement function, transforms are busted...
-    // random limits withion the plane
+    //current issues: need to set TARGET to be ignored if HUNGRY/EATING
+    // random limits withion the plane, fixed by invisible walls?
+   // needs to dynamically assign player
+
+
+    // for next demo: menu, music, eat grass, eat zebra
 
 public enum herdState
 {
@@ -36,17 +40,24 @@ public enum herdState
     EATING,
     DEAD
 }
+public static class Globals
+{
+    public const int full_hun = 512; // Unmodifiable
+}
+
 
 
 public class HerdController : MonoBehaviour
 {
     // public ThirdPersonCharacter Herd { get; private set; } // the character we are controlling
     herdState nherdState;
-    int hunger = 60;
+    int hunger = Globals.full_hun;
     int eating = 1000;
     private Transform target;                                    // target to aim for
     public Player player;
     public float movementSpeed;
+    public float seekSpeed;
+
     float fleeSpeed = 0.5F;
 
     //rigidbody component
@@ -54,6 +65,14 @@ public class HerdController : MonoBehaviour
 
     //collider component
     Collider col;
+
+        public float rotSpeed = 100f;
+
+    private bool isWandering = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+
 
     public void SetTarget(Transform target)
     {
@@ -75,7 +94,26 @@ public class HerdController : MonoBehaviour
         Vector3 playerDir = player.transform.position - transform.position;
         float player_angle = Vector3.Angle(playerDir, forward);
         var distance = Vector3.Distance(player.transform.position, transform.position);
-        print(distance);
+      //  print(distance);
+
+        if (isWandering == false)
+        {
+            StartCoroutine(Wander());
+        }
+        if (isRotatingRight == true)
+        {
+           transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+        }
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+        }
+        if (isWalking == true)
+        {
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+
+
         switch (nherdState)
         {
             case herdState.IDLE:
@@ -102,7 +140,8 @@ public class HerdController : MonoBehaviour
                     float grass_angle = Vector3.Angle(grassDir, forward);
                     if (grass_angle < 300.0F)
                     {
-                            transform.position = Vector3.MoveTowards(transform.position, obj.transform.position, movementSpeed);
+                        transform.LookAt(obj.transform.position, Vector3.zero);
+                        transform.position = Vector3.MoveTowards(transform.position, obj.transform.position, seekSpeed);
                           //  nherdState = herdState.EATING;
                             print("! --------------- ZEBRA EAT");
 
@@ -115,11 +154,7 @@ public class HerdController : MonoBehaviour
              break;
 
             case herdState.FLEEING:
-
-                //transform.position = Vector3.MoveTowards(transform.position, player.transform.position, movementSpeed); //move away not towards
-                Vector2 velocity = new Vector2((transform.position.x - player.transform.position.x) * fleeSpeed, 0);
-                rb.velocity = velocity;
-
+               transform.position += transform.forward * movementSpeed * Time.deltaTime;
                 if (distance>40)
                 {
                     SetTarget(null);
@@ -140,11 +175,47 @@ public class HerdController : MonoBehaviour
 
         }
     }
+
+    IEnumerator Wander()
+    {
+        int rotTime = Random.Range(1, 3);
+        int rotateWait = Random.Range(0, 1);
+        int rotateLorR = Random.Range(0, 3);
+        int walkWait = Random.Range(0, 1);
+        int walkTime = Random.Range(1, 5);
+
+        isWandering = true;
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+        yield return new WaitForSeconds(rotateWait);
+        if(rotateLorR == 1)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+        }
+        if (rotateLorR == 2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+        }
+        isWandering = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Grass"))
         {
             EatGrass(other);  
+        }
+        if (other.tag == "Predator")
+        {
+            //print("--------------------Got ate!!-----------------------");
+               Destroy(this.gameObject);
         }
     }
     private void EatGrass(Collider o)
@@ -154,6 +225,6 @@ public class HerdController : MonoBehaviour
         //turn off AWARENESS function
         //sets timer to eat and remove grass object
         Destroy(o.gameObject);
-        hunger = 100;
+        hunger = Globals.full_hun;
     }
   }
